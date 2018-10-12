5921256775815d6859744716db9dcd759429b5e4
waxmoronic <jdaustin@gmail.com>
2018-10-11 20:07:26 -0700

    started basic controls

--- public/scripts/playerControls.js
+++ public/scripts/playerControls.js
@@ -2,14 +2,81 @@ function playerControls(scene, clock, duck) {
 
     var currentState = playerState.init;
 
+    this.object = duck;
+    this.domElement = document;
+
+    this.moveForward = false;
+	this.moveBackward = false;
+	this.moveLeft = false;
+	this.moveRight = false;
+
+    this.domElement.addEventListener('keydown', this.onKeyDown.bind(this), false);
+    this.domElement.addEventListener('keyup', this.onKeyUp.bind(this), false);
+
     this.getState = function () {
 
         return currentState;
-    }
+    };
 
     this.update = function () {
 
         var elapsedTime = clock.getElapsedTime();
 
-    }
+        if ( this.moveForward ) this.object.translateZ( - ( motionUnit ) );
+		if ( this.moveBackward ) this.object.translateZ( motionUnit );
+
+		if ( this.moveLeft ) this.object.translateX( - motionUnit );
+		if ( this.moveRight ) this.object.translateX( motionUnit );
+
+		if ( this.moveUp ) this.object.translateY( motionUnit );
+		if ( this.moveDown ) this.object.translateY( - motionUnit );
+
+    };
+
+	this.onKeyDown = function ( event ) {
+
+		switch ( event.keyCode ) {
+
+			case 38: /*up*/
+			case 87: /*W*/ this.moveForward = true; break;
+
+			case 37: /*left*/
+			case 65: /*A*/ this.moveLeft = true; break;
+
+			case 40: /*down*/
+			case 83: /*S*/ this.moveBackward = true; break;
+
+			case 39: /*right*/
+			case 68: /*D*/ this.moveRight = true; break;
+
+			case 82: /*R*/ this.moveUp = true; break;
+			case 70: /*F*/ this.moveDown = true; break;
+
+		}
+
+	};
+
+	this.onKeyUp = function ( event ) {
+
+		switch ( event.keyCode ) {
+
+			case 38: /*up*/
+			case 87: /*W*/ this.moveForward = false; break;
+
+			case 37: /*left*/
+			case 65: /*A*/ this.moveLeft = false; break;
+
+			case 40: /*down*/
+			case 83: /*S*/ this.moveBackward = false; break;
+
+			case 39: /*right*/
+			case 68: /*D*/ this.moveRight = false; break;
+
+			case 82: /*R*/ this.moveUp = false; break;
+			case 70: /*F*/ this.moveDown = false; break;
+
+		}
+
+	};
+
 }

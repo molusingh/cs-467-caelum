6331411348fdd883d6e7ce71ae841c4ac7729c11
waxmoronic <jdaustin@gmail.com>
2018-10-13 13:34:26 -0700

    updated duck mesh, rotation
    - added duck.fbx and updated globals.js and levelAI.js with Igor's
    states branch versions
    - added rotation with movement

--- public/scripts/levelAI.js
+++ public/scripts/levelAI.js
@@ -3,11 +3,14 @@ function levelAI(scene, clock, currentLevel, difficulty) {
     var currentState, score;
     var levelAI, userInterface;
     var assetArray;
-    var player, duck;
+    var player;
+
+    var duck = new THREE.Object3D();
+    duck.name = "duck";
 
     determineAssets();
     loadAssets();
-    initAssets();
+    //initAssets();
     setupSubscriptions();
     //only if above successful 
     setState(levelState.ready);
@@ -21,28 +24,72 @@ function levelAI(scene, clock, currentLevel, difficulty) {
     function loadAssets() {
 
         var shadowMat = new THREE.ShadowMaterial({
-        color: 0xff0000, transparent: true, opacity: 0.5
+            color: 0xff0000, transparent: true, opacity: 0.5
         });
 
+        /*
         var geometry2 = new THREE.BoxBufferGeometry(6, 8, 6);
 
         var diffuseColor2 = new THREE.Color().setHSL(0.9, 0.5, 1 * 0.5 + 0.1);
         var material2 = new THREE.MeshLambertMaterial({
-        color: diffuseColor2,
+            color: diffuseColor2,
         });
 
         duck = new THREE.Mesh(geometry2, material2, shadowMat);
         duck.castShadow = true;
         duck.receiveShadow = true;
         duck.position.x = -20;
+        duck.position.z = -10;
         duck.position.y = 0;
         scene.add(duck);
+        */
+
+        var manager = new THREE.LoadingManager();
+
+        manager.onLoad = function () {
+            console.log("finished loading: " + duck);
+            console.log(duck.name);
+            initAssets();
+        }
+
+        var loader = new THREE.FBXLoader(manager);
+        loader.load('./geo/duck.fbx', function (object) {
+            object.traverse(function (child) {
+
+                if (child instanceof THREE.Mesh) {
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+                    child.shadowMaterial = shadowMat;
+                }
+
+            });
+            object.scale.x = 10;
+            object.scale.y = 10;
+            object.scale.z = 10;
+
+// removed to move duck to center of axis 
+            // object.position.x = -20;
+  
+
+
+            //object.castShadow = true;
+            //object.receiveShadow = true;
+            duck.add(object);
+            duck.userData = { currentDirection: "down"}
+            console.log('Current direction is ' + duck.userData.currentDirection);
+            scene.add(duck);
+            
+
+        }, undefined, function (e) {
+            console.error(e);
+        });
 
     }
 
     function initAssets() {
         //need to set up referencing
         player = new playerControls(scene, clock, duck);
+
     }
 
     function setState(state) {
--- public/scripts/playerControls.js
+++ public/scripts/playerControls.js
@@ -4,6 +4,8 @@ function playerControls(scene, clock, duck) {
 
     document.addEventListener('keydown', onKeyDown);
 
+console.log('Current direction is ' + duck.userData.currentDirection);
+
     this.getState = function () {
 
         return currentState;
@@ -20,16 +22,48 @@ function playerControls(scene, clock, duck) {
 		switch ( event.keyCode ) {
 
 			case 38: /*up*/
-			case 87: /*W*/ duck.position.x -= 10; break;
+			case 87: /*W*/ duck.position.x -= 10;
+					if (duck.userData.currentDirection != 'up') {
+						if (duck.userData.currentDirection == 'left') {duck.rotateY(-(Math.PI / 2));}
+						else if (duck.userData.currentDirection == 'down') {duck.rotateY(Math.PI);}
+						else if (duck.userData.currentDirection == 'right') {duck.rotateY(Math.PI / 2);}
+						duck.userData.currentDirection = 'up';
+						console.log('Current direction is ' + duck.userData.currentDirection);
+					}
+			break;
 
 			case 37: /*left*/
-			case 65: /*A*/ duck.position.z += 10; break;
+			case 65: /*A*/ duck.position.z += 10;
+					if (duck.userData.currentDirection != 'left') {
+						if (duck.userData.currentDirection == 'down') {duck.rotateY(-(Math.PI / 2));}
+						else if (duck.userData.currentDirection == 'right') {duck.rotateY(Math.PI);}
+						else if (duck.userData.currentDirection == 'up') {duck.rotateY(Math.PI / 2);}
+						duck.userData.currentDirection = 'left';
+						console.log('Current direction is ' + duck.userData.currentDirection);
+					}
+			break;
 
 			case 40: /*down*/
-			case 83: /*S*/ duck.position.x += 10; break;
+			case 83: /*S*/ duck.position.x += 10;
+					if (duck.userData.currentDirection != 'down') {
+						if (duck.userData.currentDirection == 'right') {duck.rotateY(-(Math.PI / 2));}
+						else if (duck.userData.currentDirection == 'up') {duck.rotateY(Math.PI);}
+						else if (duck.userData.currentDirection == 'left') {duck.rotateY(Math.PI / 2);}
+						duck.userData.currentDirection = 'down';
+						console.log('Current direction is ' + duck.userData.currentDirection);
+					}
+			break;
 
 			case 39: /*right*/
-			case 68: /*D*/ duck.position.z -= 10; break;
+			case 68: /*D*/ duck.position.z -= 10;
+					if (duck.userData.currentDirection != 'right') {
+						if (duck.userData.currentDirection == 'up') {duck.rotateY(-(Math.PI / 2));}
+						else if (duck.userData.currentDirection == 'left') {duck.rotateY(Math.PI);}
+						else if (duck.userData.currentDirection == 'down') {duck.rotateY(Math.PI / 2);}
+						duck.userData.currentDirection = 'right';
+						console.log('Current direction is ' + duck.userData.currentDirection);
+					}
+			break;
 
 			case 82: /*R*/ moveUp = true; break;
 			case 70: /*F*/ moveDown = true; break;

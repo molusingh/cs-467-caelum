c915dd23a855595e409582455760ee66ecb78787
waxmoronic <jdaustin@gmail.com>
2018-10-13 11:45:25 -0700

    Added WSAD controls
    - added eventListener for onKeyDown
    - added position changes for arrow keys and WSAD
    - hacked MapControls to pan with WSAD and move 17 pixels instead of 7

--- public/scripts/playerControls.js
+++ public/scripts/playerControls.js
@@ -2,16 +2,7 @@ function playerControls(scene, clock, duck) {
 
     var currentState = playerState.init;
 
-    this.object = duck;
-    this.domElement = document;
-
-    this.moveForward = false;
-	this.moveBackward = false;
-	this.moveLeft = false;
-	this.moveRight = false;
-
-    this.domElement.addEventListener('keydown', this.onKeyDown.bind(this), false);
-    this.domElement.addEventListener('keyup', this.onKeyUp.bind(this), false);
+    document.addEventListener('keydown', onKeyDown);
 
     this.getState = function () {
 
@@ -22,61 +13,29 @@ function playerControls(scene, clock, duck) {
 
         var elapsedTime = clock.getElapsedTime();
 
-        if ( this.moveForward ) this.object.translateZ( - ( motionUnit ) );
-		if ( this.moveBackward ) this.object.translateZ( motionUnit );
-
-		if ( this.moveLeft ) this.object.translateX( - motionUnit );
-		if ( this.moveRight ) this.object.translateX( motionUnit );
-
-		if ( this.moveUp ) this.object.translateY( motionUnit );
-		if ( this.moveDown ) this.object.translateY( - motionUnit );
-
     };
 
-	this.onKeyDown = function ( event ) {
+	function onKeyDown(event) {
 
 		switch ( event.keyCode ) {
 
 			case 38: /*up*/
-			case 87: /*W*/ this.moveForward = true; break;
+			case 87: /*W*/ duck.position.x -= 10; break;
 
 			case 37: /*left*/
-			case 65: /*A*/ this.moveLeft = true; break;
+			case 65: /*A*/ duck.position.z += 10; break;
 
 			case 40: /*down*/
-			case 83: /*S*/ this.moveBackward = true; break;
+			case 83: /*S*/ duck.position.x += 10; break;
 
 			case 39: /*right*/
-			case 68: /*D*/ this.moveRight = true; break;
+			case 68: /*D*/ duck.position.z -= 10; break;
 
-			case 82: /*R*/ this.moveUp = true; break;
-			case 70: /*F*/ this.moveDown = true; break;
+			case 82: /*R*/ moveUp = true; break;
+			case 70: /*F*/ moveDown = true; break;
 
 		}
 
-	};
-
-	this.onKeyUp = function ( event ) {
-
-		switch ( event.keyCode ) {
-
-			case 38: /*up*/
-			case 87: /*W*/ this.moveForward = false; break;
-
-			case 37: /*left*/
-			case 65: /*A*/ this.moveLeft = false; break;
-
-			case 40: /*down*/
-			case 83: /*S*/ this.moveBackward = false; break;
-
-			case 39: /*right*/
-			case 68: /*D*/ this.moveRight = false; break;
-
-			case 82: /*R*/ this.moveUp = false; break;
-			case 70: /*F*/ this.moveDown = false; break;
-
-		}
-
-	};
+	}
 
 }

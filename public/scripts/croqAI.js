29b3074c26d7ed6e0ae5b50764cc9671e1d13fd6
Anmolbir Singh Mann <anmolbirsingh12@gmail.com>
2018-10-14 23:01:34 +0000

    Added renamed files

---
+++ public/scripts/croqAI.js
@@ -0,0 +1,36 @@
+function croqAI(scene, clock, grid, croq) {
+
+    /*
+    -- subscribe
+    stun
+
+    -- check what's around you
+    grid.getSquareInfo(x,z)
+
+    -- find pray
+    grid.getActorsInRadius(position(x,z), actorType)
+
+    -- find next location 
+    path.getPath(), could return either an array of points or just the next grid location
+
+    -- must update grid after every move
+    grid.updateActor(actorID)
+    */
+
+    var currentState = croqState.init;
+
+    console.log("croq: " + croq);
+    croq.position.x += 20;
+
+    //must report all enum states
+    this.getState = function () {
+
+        return currentState;
+    }
+
+    this.update = function () {
+
+        var elapsedTime = clock.getElapsedTime();
+
+    }
+}

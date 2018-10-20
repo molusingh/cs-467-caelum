41da8c86215815b504aa4219e0ead8b1b717b750
waxmoronic <jdaustin@gmail.com>
2018-10-20 13:12:29 -0700

    Sounds
    - added sound files
    - created soundLoader function to play all sounds
    - edited playerControls to use ObjectMover

--- public/scripts/playerControls.js
+++ public/scripts/playerControls.js
@@ -33,6 +33,29 @@ function playerControls(scene, clock, duck) {
 	$("#downButton").click(duckMover.down);
 	$("#upButton").click(duckMover.up);
     
+    // sounds for interface buttons
+	var playSound = new soundLoader();
+	$("#movementControls").click(playSound.click);
+	$("#movementControls").click(playSound.move);
+	$("#skillButtons").click(playSound.click);
+	$("#actionButtons").click(playSound.click);
+	$("#startButton").click(playSound.click);
+	$("#howToPlayButton").click(playSound.click);
+	$("#menu").click(playSound.click);
+	$("#menuButton").click(playSound.click);
+
+	// skill sounds
+	$("#invisibilityButton").click(playSound.invisiblity);
+	$("#speedButton").click(playSound.speedBoost);
+	$("#quackButton").click(playSound.superQuack);
+
+	// action sounds
+	$("#flyButton").click(playSound.fly);
+	$("#jumpButton").click(playSound.jump);
+	$("#callButton").click(playSound.call);
+	$("#nestButton").click(playSound.nest);
+
+
 
 
     document.addEventListener('keydown', onKeyDown);
@@ -49,49 +72,39 @@ function playerControls(scene, clock, duck) {
     };
 
     function onKeyDown(event) {
-
+ 
         switch (event.keyCode) {
 
             case 38: /*up*/
-            case 87: /*W*/  if (duck.position.x >= -maxPos) { duck.position.x -= 10; }
-                if (duck.userData.currentDirection != 'up') {
-                    if (duck.userData.currentDirection == 'left') { duck.rotateY(-(Math.PI / 2)); }
-                    else if (duck.userData.currentDirection == 'down') { duck.rotateY(Math.PI); }
-                    else if (duck.userData.currentDirection == 'right') { duck.rotateY(Math.PI / 2); }
-                    duck.userData.currentDirection = 'up';
-
-                }
+            case 87: /*W*/  
+            	if (duck.position.x >= -maxPos) { 
+            		duckMover.up();
+            		playSound.move();
+            	}
                 break;
 
             case 37: /*left*/
-            case 65: /*A*/ if (duck.position.z <= maxPos) { duck.position.z += 10 };
-                if (duck.userData.currentDirection != 'left') {
-                    if (duck.userData.currentDirection == 'down') { duck.rotateY(-(Math.PI / 2)); }
-                    else if (duck.userData.currentDirection == 'right') { duck.rotateY(Math.PI); }
-                    else if (duck.userData.currentDirection == 'up') { duck.rotateY(Math.PI / 2); }
-                    duck.userData.currentDirection = 'left';
-                }
+            case 65: /*A*/ 
+            	if (duck.position.z <= maxPos) { 
+            		duckMover.left();
+            		playSound.move();
+            	}
                 break;
 
             case 40: /*down*/
-            case 83: /*S*/ if (duck.position.x <= maxPos) { duck.position.x += 10 };
-                if (duck.userData.currentDirection != 'down') {
-                    if (duck.userData.currentDirection == 'right') { duck.rotateY(-(Math.PI / 2)); }
-                    else if (duck.userData.currentDirection == 'up') { duck.rotateY(Math.PI); }
-                    else if (duck.userData.currentDirection == 'left') { duck.rotateY(Math.PI / 2); }
-                    duck.userData.currentDirection = 'down';
-
-                }
-                break;
+            case 83: /*S*/ 
+            	if (duck.position.x <= maxPos) { 
+            		duckMover.down();
+            		playSound.move();
+            	}
+            	break;
 
             case 39: /*right*/
-            case 68: /*D*/ if (duck.position.z >= -maxPos) { duck.position.z -= 10 };
-                if (duck.userData.currentDirection != 'right') {
-                    if (duck.userData.currentDirection == 'up') { duck.rotateY(-(Math.PI / 2)); }
-                    else if (duck.userData.currentDirection == 'left') { duck.rotateY(Math.PI); }
-                    else if (duck.userData.currentDirection == 'down') { duck.rotateY(Math.PI / 2); }
-                    duck.userData.currentDirection = 'right';
-                }
+            case 68: /*D*/ 
+            	if (duck.position.z >= -maxPos) { 
+            		duckMover.right();
+            		playSound.move();
+            	}
                 break;
 
             case 82: /*R*/ moveUp = true; break;

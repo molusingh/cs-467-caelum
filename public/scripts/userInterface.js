fdcf4e81b2e4667f456e178e761236328fb6891f
Anmolbir Singh Mann <anmolbirsingh12@gmail.com>
2018-10-23 00:47:31 +0000

    Starting gameAI, adding some event bus events

--- public/scripts/userInterface.js
+++ public/scripts/userInterface.js
@@ -1,11 +1,13 @@
 /* global $*/
 /* global location*/
+/* global bus*/
 
 /*
  * Simply create instance of object to activate user interface
  */
-function userInterface()
+function UserInterface()
 {
+	// document.addEventListener('keydown', onKeyDown);
 	$("#menuButton").click(flipMenuDisplay);
 	$("#closeMenuButton").click(flipMenuDisplay);
 	$("#restartButton").click(restart);
@@ -16,6 +18,7 @@ function userInterface()
 	 */
 	function start()
 	{
+		bus.publish("start");
 		$("#startScreen").hide();
 	}
 
@@ -32,6 +35,7 @@ function userInterface()
 	 */
 	function flipMenuDisplay(event)
 	{
+		bus.publish("menu");
 		if ($("#menu").css("display") == "none")
 		{
 			$("#menu").show();
@@ -39,6 +43,42 @@ function userInterface()
 		else
 		{
 			$("#menu").hide();
+		}
+	}
+
+	function onKeyDown(event)
+	{
+
+		switch (event.keyCode)
+		{
+
+			case 38: // up
+			case 87: // W
+				bus.publish("moveUp");
+				break;
+
+			case 37: // left
+			case 65: // A
+				bus.publish("moveLeft");
+				break;
+
+			case 40: // down
+			case 83: // S
+				bus.publish("moveDown");
+				break;
+
+			case 39: // right
+			case 68: // D
+				bus.publish("moveRight");
+				break;
+
+			case 82: // R
+				moveUp = true;
+				break;
+				
+			case 70: // F
+				moveDown = true;
+				break;
 		}
 	}
 }
